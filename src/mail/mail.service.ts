bc4ee15ce1bf8d7e04b5fd85b99731ffd11a2fef
aceiny <a_zeraibi@estin.dz>
2024-12-19 22:43:39 +0100

    mail : setup nodemailer for gmail and commented smtp server related code

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -6,9 +6,12 @@ export class MailService {
   private transporter: nodemailer.Transporter;
     constructor(){
         this.transporter = nodemailer.createTransport({
+            /*
             host: process.env.SMTP_HOST,
             port: 465,
             secure: true,
+            */
+            service : 'gmail',
             auth: {
               user: process.env.SMTP_USER,
               pass: process.env.SMTP_PASSWORD,

0afe3d831db6abd79a1178498d8e37c1d658ec3f
aceiny <yzeraibi2000@estin.dz>
2024-12-25 03:21:45 +0100

    add more swagger docs

--- src/chat/dto/create-chat.dto.ts
+++ src/chat/dto/create-chat.dto.ts
@@ -1,6 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsUUID } from 'class-validator';
 
 export class CreateChatDto {
+
+  @ApiProperty({
+    description: 'User ID of the user',
+    type: String,
+    required: true,
+    example: '123e4567-ee89-12d3-a456-426614174000',
+  })
   @IsUUID()
   user: string;
 }
--- src/user/dto/create-user-profile.dto.ts
+++ src/user/dto/create-user-profile.dto.ts
@@ -6,18 +6,44 @@ import {
   IsString,
 } from 'class-validator';
 import { EmploymentStatus } from './user.schema';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserProfileDto {
+
+  @ApiProperty({
+    description: 'Username of the user',
+    type: String,
+    required: true,
+    example: 'john_doe',
+  })
   @IsString()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty({
+    description : "User's birthday",
+    type: Date,
+    required: true,
+    example: '1990-01-01',
+  })
   @IsDateString()
   birthday: Date;
 
+  @ApiProperty({
+    description: 'Employment status of the user',
+    type: String,
+    required: true,
+    example: 'Employed',
+  })
   @IsEnum(EmploymentStatus)
   employment_status: EmploymentStatus;
 
+  @ApiProperty({
+    description: 'Bio of the user',
+    type: String,
+    required: false,
+    example: 'I am a software engineer',
+  })
   @IsOptional()
   @IsString()
   bio: string;

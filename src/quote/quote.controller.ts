869c9e7422654ab115f5b73ae8a1a39f0e0a7d37
aceiny <yzeraibi2000@gmail.com>
2025-01-27 19:06:43 +0100

    feat: add API operation summaries to quote controller methods

--- src/quote/quote.controller.ts
+++ src/quote/quote.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { QuoteService } from './quote.service';
 import { CreateQuoteDto, CreateQuoteRequestDto } from './dto/create-quote.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { User, UserRole } from 'src/user/dto/user.schema';
 import { GetUser } from 'src/auth/decorators/get-user.decorator';
@@ -12,6 +12,9 @@ import { Roles } from 'src/auth/decorators/roles.decorator';
 export class QuoteController {
   constructor(private readonly quoteService: QuoteService) {}
 
+  @ApiOperation({
+    summary : "request a new job quote"
+  })
   @Post('request')
   @UseGuards(JwtAuthGuard)
   async createQuoteRequest(
@@ -20,6 +23,7 @@ export class QuoteController {
   ) {
     return this.quoteService.createQuoteRequest(createQuoteRequestDto , user.user_id);
   }
+
 
   @Post()
   @UseGuards(JwtAuthGuard , RolesGuard)
@@ -32,16 +36,25 @@ export class QuoteController {
   }
 
   @Get('job/:jobId/requests')
+  @ApiOperation({
+    summary : "view  job all quotes"
+  })
   async getQuoteRequestsByJob(@Param('jobId') jobId: string) {
     return this.quoteService.getQuoteRequestsByJob(jobId);
   }
 
+  @ApiOperation({
+    summary : " view all quotes for a request"
+  })
   @Get('request/:requestId/quotes')
   async getQuotesByRequest(@Param('requestId') requestId: string) {
     return this.quoteService.getQuotesByRequest(requestId);
   }
 
   @Patch(':quoteId/status')
+  @ApiOperation({
+    summary : "update quote status (accepted , rejected)"
+  })
   @UseGuards(JwtAuthGuard)
   async updateQuoteStatus(
     @Param('quoteId') quoteId: string,
